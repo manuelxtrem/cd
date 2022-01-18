5fe75e8866111c7bb83266cd396729be9fe4525f
Emmanuel <manuelxtrem@users.noreply.github.com>
2022-01-18 10:11:53 +0000

    test

--- .github/scripts/determine_version.sh
+++ .github/scripts/determine_version.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-# if VERSION_NEW
+# if branch looks like v*.*, get the *.*, and create a version from it
 
-echo $SRC_BRANCH
+export src=$SRC_BRANCH
+
+major=0
+minor=0
+
+regex="refs/heads/v([0-9]+).([0-9]+)"
+if [[ $src =~ $regex ]]; then
+  major="${BASH_REMATCH[1]}"
+  minor="${BASH_REMATCH[2]}"
+fi
+
+VERSION_NEW=""
+
+if [ "$minor" == "0" ]; then 
+  VERSION_NEW=$VERSION_PATCH
+else
+  if [ "$major" == "0" ]; then 
+    VERSION_NEW=$VERSION_MINOR
+  else
+    VERSION_NEW=$VERSION_MAJOR
+  fi;
+fi;
+
+echo VERSION_NEW=$VERSION_NEW >> $GITHUB_ENV
